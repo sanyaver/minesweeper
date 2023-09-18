a55bfd239bb2acee8ac6f686ebdde841d780e7d5
sanyaver <sanyaver@usc.edu>
2023-09-19 03:07:45 +0530

    Update DisplayActivity.java

--- app/src/main/java/com/example/gridlayout/DisplayActivity.java
+++ app/src/main/java/com/example/gridlayout/DisplayActivity.java
@@ -1,53 +1,22 @@
-package com.example.gridlayout;
-
-import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
 import com.example.gridlayout.databinding.ActivityDisplayBinding;
 
 public class DisplayActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityDisplayBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
 
-        binding = ActivityDisplayBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("com.example.GridLayout.MESSAGE");
 
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_display);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_display);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    public void backToMain(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
+
 }
